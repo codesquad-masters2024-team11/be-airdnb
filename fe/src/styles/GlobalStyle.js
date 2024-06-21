78b4934499306939f88109796af2f6dfe3184a60
최호빈 <hobinchoi0116@gmail.com>
2024-06-21 11:42:53 +0900

    feat: 전역적으로 사용하는 css 추가

---
+++ fe/src/styles/GlobalStyle.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+`;
+
+export default GlobalStyle;
